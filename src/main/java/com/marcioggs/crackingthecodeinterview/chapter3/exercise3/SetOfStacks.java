c9bc8d82791a592c34f0e2e76b9f3268ec86594c
GOMES DA SILVA Marcio Gabriel <marciogabriel.gomesdasilva@ext.europarl.europa.eu>
2021-02-11 18:48:33 +0100

    Add Cracking the code interview - Chapter 3 - Exercise 3

---
+++ src/main/java/com/marcioggs/crackingthecodeinterview/chapter3/exercise3/SetOfStacks.java
@@ -0,0 +1,46 @@
+package com.marcioggs.crackingthecodeinterview.chapter3.exercise3;
+
+import java.util.*;
+
+public class SetOfStacks<T> {
+
+  private List<Stack<T>> stacks;
+
+  private int stackLimit;
+
+  public SetOfStacks(int stackLimit) {
+    if (stackLimit <= 0) {
+      throw new IllegalArgumentException();
+    }
+    this.stacks = new ArrayList<>();
+    this.stacks.add(new Stack<>());
+    this.stackLimit = stackLimit;
+  }
+
+  public T push(T item) {
+    Stack<T> lastStack = this.getLastStack();
+
+    if (lastStack.size() == this.stackLimit) {
+      lastStack = new Stack<>();
+      this.stacks.add(lastStack);
+    }
+
+    return lastStack.push(item);
+  }
+
+  private Stack<T> getLastStack() {
+    return this.stacks.get(this.stacks.size() - 1);
+  }
+
+  public T pop() {
+    Stack<T> lastStack = this.getLastStack();
+
+    T popped = lastStack.pop();
+
+    if (lastStack.size() == 0 && this.stacks.size() > 1) {
+      this.stacks.remove(lastStack);
+    }
+
+    return popped;
+  }
+}
---
+++ src/test/java/com/marcioggs/crackingthecodeinterview/chapter3/exercise3/SetOfStacksTest.java
@@ -0,0 +1,51 @@
+package com.marcioggs.crackingthecodeinterview.chapter3.exercise3;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.EmptyStackException;
+
+import static org.junit.Assert.assertEquals;
+
+public class SetOfStacksTest {
+
+  @Test
+  public void singleStackShouldWork() {
+    SetOfStacks<Integer> setOfStacks = new SetOfStacks<>(100);
+
+    Integer firstItem = 1;
+    Integer secondItem = 2;
+
+    setOfStacks.push(firstItem);
+    setOfStacks.push(secondItem);
+
+    assertEquals(secondItem, setOfStacks.pop());
+    assertEquals(firstItem, setOfStacks.pop());
+  }
+
+  @Test
+  public void popWithoutElementsShouldThrowException() {
+    SetOfStacks<Integer> setOfStacks = new SetOfStacks<>(100);
+    Assert.assertThrows(EmptyStackException.class, setOfStacks::pop);
+  }
+
+  @Test
+  public void multipleStacksShouldWork() {
+    SetOfStacks<Integer> setOfStacks = new SetOfStacks<>(2);
+
+    Integer firstItem = 1;
+    Integer secondItem = 2;
+    Integer thirdItem = 3;
+    Integer fourthItem = 4;
+
+    setOfStacks.push(firstItem);
+    setOfStacks.push(secondItem);
+    setOfStacks.push(thirdItem);
+    setOfStacks.push(fourthItem);
+
+    assertEquals(fourthItem, setOfStacks.pop());
+    assertEquals(thirdItem, setOfStacks.pop());
+    assertEquals(secondItem, setOfStacks.pop());
+    assertEquals(firstItem, setOfStacks.pop());
+  }
+}
