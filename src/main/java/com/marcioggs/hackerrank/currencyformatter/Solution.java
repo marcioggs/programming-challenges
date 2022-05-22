3834bc3a30ae243ba3927502c35f531bfdf7be77
Márcio Gabriel <marcioggs@gmail.com>
2022-05-22 17:53:41 +0200

    Add HackerRank - Currency problem

---
+++ src/main/java/com/marcioggs/hackerrank/currencyformatter/Solution.java
@@ -0,0 +1,20 @@
+package com.marcioggs.hackerrank.currencyformatter;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+/**
+ * https://www.hackerrank.com/challenges/java-currency-formatter/problem
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+
+        double payment = 12324.134;
+
+        System.out.println("US: " + NumberFormat.getCurrencyInstance(Locale.US).format(payment));
+        System.out.println("India: " + NumberFormat.getCurrencyInstance(new Locale("en", "IN")).format(payment));
+        System.out.println("China: " + NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment));
+        System.out.println("France: " + NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment));
+    }
+}
