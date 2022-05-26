e23437b518be6f80acc7189590a5ff46bf291a6f
Márcio Gabriel <marcioggs@gmail.com>
2022-05-26 20:00:14 +0200

    Add Codility - Exercise 2 - Rectangle builder greater area

---
+++ src/test/java/com/marcioggs/codility/trainings/exercise2/rectanglebuildergreaterarea/SolutionTest.java
@@ -0,0 +1,20 @@
+package com.marcioggs.codility.trainings.exercise2.rectanglebuildergreaterarea;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    @Test
+    public void test() {
+        assertEquals(2, new Solution().solution(new int[]{1, 2, 5, 1, 1, 2, 3, 5, 1}, 5));
+        assertEquals(0, new Solution().solution(new int[]{}, 5));
+        assertEquals(0, new Solution().solution(new int[]{1, 2}, 5));
+        assertEquals(0, new Solution().solution(new int[]{1, 2, 2}, 5));
+        assertEquals(1, new Solution().solution(new int[]{2, 2, 2, 2}, 1));
+        assertEquals(3, new Solution().solution(new int[]{2, 2, 2, 2, 2, 4, 4, 4, 4}, 1));
+        assertEquals(0, new Solution().solution(new int[]{5, 5, 3, 3}, 16));
+        assertEquals(1, new Solution().solution(new int[]{5, 5, 3, 3}, 15));
+    }
+}
