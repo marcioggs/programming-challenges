5ea5d5e729283b45d9fe59f791c162b9bf926345
Márcio Gabriel <marcioggs@gmail.com>
2022-06-08 17:36:27 +0200

    Add Codility - Lesson 1

---
+++ src/main/java/com/marcioggs/codility/lessons/lesson1/Solution.java
@@ -0,0 +1,32 @@
+package com.marcioggs.codility.lessons.lesson1;
+
+/**
+ * https://app.codility.com/programmers/lessons/1-iterations/binary_gap/
+ */
+public class Solution {
+
+    public int solution(int number) {
+        int longestGap = 0;
+        int currentGap = 0;
+        boolean gapStarted = false;
+
+        for (char currentChar : Integer.toBinaryString(number).toCharArray()) {
+            boolean isZero = currentChar == '0';
+
+            if (!gapStarted && isZero) {
+                continue;
+            } else {
+                gapStarted = true;
+            }
+
+            if (isZero) {
+                currentGap++;
+            } else {
+                longestGap = Math.max(currentGap, longestGap);
+                currentGap = 0;
+            }
+        }
+
+        return longestGap;
+    }
+}
