ab38f9e304f3df84200986782d05e32ebdee8c71
Márcio Gabriel <marcioggs@gmail.com>
2023-02-15 18:38:45 +0100

    Add 1 exercises for Cracking the Coding Interview

---
+++ src/main/java/com/marcioggs/crackingthecodeinterview/chapter10/exercise2/Solution.java
@@ -0,0 +1,31 @@
+package com.marcioggs.crackingthecodeinterview.chapter10.exercise2;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String[] words = new String[] {"aabb", "aacc", "bbaa", "ccaa"};
+        new Solution().groupAnagrams(words);
+
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+
+    public void groupAnagrams(String[] words) {
+        Arrays.sort(words, new AnagramComparator());
+    }
+
+    private static class AnagramComparator implements Comparator<String> {
+        public int compare(String word1, String word2) {
+            return getSortedLetters(word1).compareTo(getSortedLetters(word2));
+        }
+
+        private String getSortedLetters(String word) {
+            char[] sortedLetters = word.toCharArray();
+            Arrays.sort(sortedLetters);
+            return Arrays.toString(sortedLetters);
+        }
+    }
+}
